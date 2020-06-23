b41e3682f08955dc1cdec52353624647d07b72b1
Michael T. Kloos <michael@michaelkloos.com>
2020-06-23 11:10:11 -0400

    BUGFIX: The ASM setup of the main function parameters was wrong. The solution is actually far simpler.  The format is already correct from the kernel.  All that is needed is to get 3 function parameters off the stack.  I fixed it in "caller.s".
    I changed the stack pointer alignment to be more robust.  While, I
    do not think this is necessary, it now has the logic to fix the
    stack pointer to a 16 byte alignment even if the stack pointer is off
    by some non-zero value which fits inside the bitmask 0b111.  Or, in
    other words, if the stack pointer is not even aligned 8 byte bounds,
    The new code should fix it.  However, I don't think that is possible
    in 64-bit mode.  It is there now, just in case, and doesn't add any
    complexity or code to implement.

--- caller.s
+++ caller.s
@@ -13,91 +13,24 @@ my_entry_pt:
   cmp %rdi, %rdx
   jc get_argv
   mov $0, %rsi
+  mov %rsp, %rax
   jmp get_envp
-  
-  # Use the envp pointer to demark the cstr termination point
-  adjust_terminator:
-  add $8, %r9
-  mov (%r9), %r11
-  jmp ret_pt
   
   # Get and copy to add NULL terminator to argv (Located: Just under argc on the stack)
   get_argv:
+  mov %rsp, %rsi
   mov $8, %rax
   mul %rdi
-  mov %rsp, %r8
-  sub %rax, %rsp
-  mov %rsp, %rcx
-  mov %rcx, %rsi
-  
-  copy_argv_cstr:
-  mov (%r8), %r10
-  cmp $0, %r10
-  jz get_envp
-  mov %r8, %r9
-  add $8, %r9
-  mov (%r9), %r11
-  cmp $0, %r11
-  jz adjust_terminator
-  ret_pt:
-  
-  cmp %r11, %r10
-  jc write_char_loop_start
-  push $0  
-  jmp exit_write_char_loop
-  
-  write_char_loop_start:
-    
-  mov %r11, %rax
-  sub %r10, %rax
-  xor %rdx, %rdx
-  mov $8, %r9
-  div %r9
-  cmp $0, %rdx
-  jnz not_even_alignment
-  push $0
-  not_even_alignment:
-  sub %rdx, %r11
-  mov (%r11), %r9
-  xor %rax, %rax
-  bitmask_loop:
-  shl $8, %rax
-  add $0xFF, %rax
-  dec %rdx
-  cmp $0, %rdx
-  jnz bitmask_loop
-  and %rax, %r9
-  push %r9
-  
-  write_char_loop:
-  
-  cmp %r11, %r10
-  jnc exit_write_char_loop
-  
-  sub $8, %r11
-  mov (%r11), %r9
-  push %r9
-  
-  jmp write_char_loop
-  
-  exit_write_char_loop:
-  mov %rsp, (%rcx)
-  add $8, %rcx
-  
-  add $8, %r8
-  jmp copy_argv_cstr
+  add %rsp, %rax
   
   # Get envp (Located: At the end of argv + 8 zero bytes [argc * 8 + 8], on the stack)
   get_envp:
-  mov $8, %rax
-  mul %rdi
   add $8, %rax
-  add %rax, %rcx
-  mov %rcx, %rdx
+  mov %rax, %rdx
   
   # Ensure the stack pointer is aligned to a 16 byte boundry
   mov %rsp, %rax
-  and $8, %rax
+  and $0xF, %rax
   sub %rax, %rsp
   
   # Call my C function
